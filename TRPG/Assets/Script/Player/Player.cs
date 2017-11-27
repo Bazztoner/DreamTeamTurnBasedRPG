b082dade1c71fda2573f2d28c514a242ba8d1fd7
Daydam <martin.daydam@gmail.com>
2017-11-27 03:22:20 -0300

    Agregada la carpeta Player
    Agregado el script player y statcontrol. ambos scritps tienen que estar en el personaje para controlar los stats y skills correctamente.

---
+++ TRPG/Assets/Script/Player/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(StatControl))]
+public class Player : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int maxEnergy = 5;
+    public float rechargeTime = 5;
+    float timeLeftToRecharge;
+    int hp;
+    int energy;
+    public int Energy { get { return energy; } set { energy = Mathf.Max(Mathf.Min(value, maxEnergy), 0); } }
+    StatControl sC;
+    public StatControl SC { get { return sC; } }
+
+	void Start ()
+    {
+        sC = GetComponent<StatControl>();
+        hp = maxHP;
+        energy = maxEnergy;
+
+        rechargeTime -= rechargeTime * sC.CurrentStats[StatType.SPEED] / 100;
+        timeLeftToRecharge = rechargeTime;
+	}
+	
+	void Update ()
+    {
+		if(energy < maxEnergy)
+        {
+            timeLeftToRecharge -= Time.deltaTime;
+            if(timeLeftToRecharge <= 0)
+            {
+                float criticalRecharge = Random.Range(0,1);
+                // Divide by 200: 100 for percentage,
+                //multiplied by 2 to make sure that a 100 luck stat gives 50% chance of critical restoration
+                if (criticalRecharge > sC.CurrentStats[StatType.LUCK] / 200) energy = Mathf.Min(maxEnergy, energy + 2);
+                else energy++;
+                timeLeftToRecharge = rechargeTime;
+            }
+        }
+	}
+}
---
+++ TRPG/Assets/Script/Player/StatControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatControl : MonoBehaviour
+{
+    Stats originalStats;
+    Stats currentStats;
+    public Stats CurrentStats { get { return currentStats; } }
+    
+    void Start ()
+    {
+        originalStats = new Stats().SetLevel(1).SetExperience(0).SetStrength(15).SetMarksmanship(12).SetDexterity(9).SetLuck(7).SetSpeed(14);
+        currentStats = originalStats;
+        EventManager.Subscribe(EventID.PLAYER_BUFFED, OnBuff);
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnBuff(params object[] info)
+    {
+        if (this == (StatControl)info[0])
+        {
+            currentStats = new Buff(currentStats, (Stats)info[1]);
+            StartCoroutine(ResetStats((Buff)currentStats, (float)info[2]));
+        }
+    }
+
+    IEnumerator ResetStats(Buff buff, float time)
+    {
+        var timeToClean = new WaitForSecondsRealtime(time);
+        yield return timeToClean;
+
+        Buff c = (Buff)currentStats;
+        if (c == buff) currentStats = c.StoredStats;
+        else
+        {
+            while (c.StoredStats != originalStats)
+            {
+                if (c.StoredStats == buff) c.StoredStats = ((Buff)c.StoredStats).StoredStats;
+                else c = (Buff)c.StoredStats;
+            }
+        }
+    }
+}
